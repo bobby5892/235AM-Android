c446f90eb11d8b0c7ceef70bbbb1cea47a50b561
Robert Moore <robert.ben.moore@gmail.com>
2018-09-29 14:40:06 -0700

    Initial

---
+++ Lab1A-robertMoore/Tutorial-C/Tutorial-C/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Tutorial_C
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //Create the user interface in code
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+            var aLabel = new TextView(this);
+            //aLabel.Text = "Hello, Xamarin.Android";
+            aLabel.SetText(Resource.String.helloLabelText);
+            var aButton = new Button(this);
+            //aButton.Text = "Say Hello";
+            aButton.SetText(Resource.String.helloButtonText);
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+            };
+            var aButtonReset = new Button(this);
+            //aButton.Text = "Say Hello";
+            aButtonReset.SetText(Resource.String.resetButtonText);
+            aButtonReset.Click += (sender, e) => {
+                aLabel.Text = "Hello Xamarin.Android";
+            };
+            
+            layout.AddView(aButton);
+            layout.AddView(aButtonReset);
+            layout.AddView(aLabel);
+            SetContentView(layout);
+        }
+    }
+}
---
+++ Lab1A-rovertMoore-XML/Tutorial XML/Tutorial XML/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Tutorial_XML
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+
+            var resetButton = FindViewById<Button>(Resource.Id.resetButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+            };
+            resetButton.Click += (sender, e) => {
+                aLabel.Text = "Welcome to Xamarin";
+            };
+
+        }
+    }
+}
