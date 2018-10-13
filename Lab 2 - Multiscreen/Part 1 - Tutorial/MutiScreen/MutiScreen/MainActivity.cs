2afc0c5d5609f3ca6ce88c5a7f6c8946db57c308
Robert Moore <robert.ben.moore@gmail.com>
2018-10-13 12:39:40 -0700

    Lab 3
    Lab3 Done

---
+++ Lab 2 - Multiscreen/Part 1 - Tutorial/MutiScreen/MutiScreen/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace MutiScreen
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Use UI created in Main.axml
+            SetContentView(Resource.Layout.activity_main);
+            var showSecond = FindViewById<Button>(Resource.Id.secondActivity);
+
+            showSecond.Click += (sender, e) => {
+                var second = new Intent(this, typeof(SecondActivity));
+                second.PutExtra("FirstData", "Data from FirstActivity");
+                StartActivity(second);
+            };
+            
+
+        }
+    }
+}
---
+++ Lab2-Multiscreen/Poke/secondActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Poke
+{
+    [Activity(Label = "secondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.secondActivity);
+            var label = FindViewById<TextView>(Resource.Id.screen2Message);
+            label.Text = Intent.GetStringExtra("secondText") ?? "Data not available";
+            // Create your application here
+        }
+    }
+}
